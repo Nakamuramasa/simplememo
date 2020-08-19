e4a595826597b8600569c8bd3de62353288a3a29
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-19 22:30:57 +0900

    Created UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'username' => $this->username,
+            'email' => $this->email,
+            'create_dates' => [
+                'created_at_human' => $this->created_at->diffForHumans(),
+                'created_at' => $this->created_at
+            ],
+            'update_dates' => [
+                'update_at_human' => $this->updated_at->diffForHumans(),
+                'updated_at' => $this->updated_at
+            ]
+        ];
+    }
+}
