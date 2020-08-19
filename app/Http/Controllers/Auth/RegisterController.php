006ec4a866be91da63b5b7d5d80d4d17f1d585ba
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-19 16:43:49 +0900

    Created Register Verification methods

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,43 +2,21 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use App\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected function registered(Request $request, User $user)
     {
-        $this->middleware('guest');
+        return response()->json($user, 200);
     }
 
     /**
@@ -50,7 +28,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'min:3', 'max:16'],
+            'username' => ['required', 'string', 'alpha_num', 'min:3', 'max:25', 'unique:users,username'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -66,6 +45,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
-Route::get('/', function(){
-    return response()->json(['message' => 'Hello World'], 200);
+Route::group(['middleware' => ['auth:api']], function (){
+
+});
+
+Route::group(['middleware' => ['guest:api']], function (){
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::post('verification/verify', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('verification/resend', 'Auth\VerificationController@resend');
 });
